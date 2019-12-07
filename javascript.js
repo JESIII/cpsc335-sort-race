f2d4c6b7b45c600bed4018cde2b79b6fff08c982
John S <tinco7@aol.com>
2019-12-07 00:09:39 -0800

    stuff

--- javascript.js
+++ javascript.js
@@ -78,25 +78,39 @@ function swap(obj, leftIndex, rightIndex){
 /////////////
 //QUICKSORT//
 /////////////
-function QuickSort(obj, low, high) {
-    if(low<high){
-      obj.pivot = QSPart(obj,low,high)
+function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        while (items[i] < pivot) {
+            i++;
+        }
+        while (items[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
     }
+    return i;
 }
-function QSPart(obj,low,high){
-  var pivot = obj.items[high],
-  i = low-1;
-  for(var j = low; j<= high-1;j++){
-    if(obj.items[j]<pivot){
-      i++
-      obj.i = i
-      obj.j = j
-      swap(obj,i,j)
-      return
+
+function quickSort(obj,items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+          obj.nextop = 1
+          //quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+          obj.nextop = 2
+          //quickSort(items, index, right);
+        }
     }
-  }
-  swap(obj,i+1,high)
-  return i+1
 }
 ///////////////
 //BUBBLESORT//
