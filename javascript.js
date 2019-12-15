a58690fc64e6c8103a517cdb65c5124fc0052190
John S <tinco7@aol.com>
2019-12-14 17:47:55 -0800

    working

--- javascript.js
+++ javascript.js
@@ -9,6 +9,100 @@ Whittier, CA 90601
 This file contains all of the js for project 3.
 */
 //convert hex to base10
+function inArray(key, arr){
+  for(var i = 0; i<arr.length; i++){
+    if(key == arr[i]){
+      return true
+    }
+  }
+  return false
+}
+function drawBubbleText( ctx, rtext, x,y, color )
+{
+    ctx.save( );
+    cx1.fillStyle = color || 'lightgrey';
+    cx1.font = "20px Arial";
+    cx1.fillText( rtext, x, y);
+    ctx.restore( );
+}
+function drawBubbleArray(arr, step){
+  var location = 0;
+  if(step!=1){
+    drawBubbleText(cx1, "Pass "+(step-1)+": ", 1,step*25+20)
+  }else{
+    drawBubbleText(cx1, "Start: ", 1,step*25+20)
+  }
+
+  for(var  i = 0; i<arr.length; i++){
+    if(arr[i]>9){
+      if(inArray(arr[i], bbswap)){
+        drawBubbleText(cx1, arr[i], location*18+90,step*25+20, 'green')
+      }else{
+        drawBubbleText(cx1, arr[i], location*18+90,step*25+20)
+      }
+
+      location=location+.5
+    }else{
+      if(inArray(arr[i], bbswap)){
+        drawBubbleText(cx1, arr[i], location*18+90,step*25+20, 'green')
+      }else{
+        drawBubbleText(cx1, arr[i], location*18+90,step*25+20)
+      }
+
+    }
+    location++;
+  }
+}
+function drawMrgText( ctx, rtext, x,y, color )
+{
+  var newcolor = 'lightgrey';
+  if(color % 2 == 0){
+    newcolor = 'lightgrey'
+  }else{
+    newcolor = 'green'
+  }
+  ctx.save( );
+  cx2.fillStyle = newcolor || 'lightgrey';
+  cx2.font = "20px Arial";
+  cx2.fillText( rtext, x, y);
+  ctx.restore( );
+}
+function drawMrgArray(arr, step){
+  var temparray = []
+  var color = 0
+  if(step==0){
+    drawMrgText(cx2, "Start: ", 1,step+45,0)
+
+  }else{
+    drawMrgText(cx2, "Pass "+(step-1)+": ", 1,step*25+20,0)
+  }
+  for(var  i = 0; i<arr.length; i++){
+    for(var j = 0; j<arr[i].length; j++){
+      temparray.push({value: arr[i][j], color: color})
+    }
+    color++
+  }
+  var location = 0;
+  for(var i = 0; i<temparray.length; i++){
+    console.log(temparray[i])
+    if(step == 0){
+      if(temparray[i].value > 9){
+        drawMrgText(cx2, temparray[i].value, location*18+80,step*25+45, temparray[i].color)
+        location=location+.6
+      }else{
+        drawMrgText(cx2, temparray[i].value, location*18+80,step*25+45, temparray[i].color)
+      }
+    }else{
+      if(temparray[i].value > 9){
+        drawMrgText(cx2, temparray[i].value, location*18+80,step*25+20, temparray[i].color)
+        location=location+.6
+      }else{
+        drawMrgText(cx2, temparray[i].value, location*18+80,step*25+20, temparray[i].color)
+      }
+    }
+    location++
+  }
+}
 function hextonum(aa){
   for (var i = 0; i<aa.length; i++){
     //console.log(aa[i])
@@ -70,6 +164,16 @@ function hextonum(aa){
 //SWAP FUNCTION//
 /////////////////
 function swap(obj, leftIndex, rightIndex){
+  if(obj.name == "bb"){
+    bbswap.push(obj.items[leftIndex])
+    bbswap.push(obj.items[rightIndex])
+  }else if(obj.name == "mrg"){
+    mrgswap.push(obj.items[leftIndex])
+    mrgswap.push(obj.items[rightIndex])
+  }else if(obj.name == "qs"){
+    qsswap.push(obj.items[leftIndex])
+    qsswap.push(obj.items[rightIndex])
+  }
     console.log("Swap "+obj.items[leftIndex] + " with " + obj.items[rightIndex])
     var temp = obj.items[leftIndex];
     obj.items[leftIndex] = obj.items[rightIndex];
@@ -130,7 +234,7 @@ function BubbleSort(obj, ctr){
       obj.swap++
       swap(obj,j,j+1)
       obj.curpos=j+1
-      break
+      //break
     }
   }
   if(j==obj.items.length-1-obj.ctr){
